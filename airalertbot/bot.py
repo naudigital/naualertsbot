f6d0c1aeb7b2dc9371fbb93436d33db382711ac7
Stanislav <h3xcode@envs.net>
2023-06-27 12:48:00 +0300

    bot: properly handle webhook_url when base_url has path

--- airalertbot/bot.py
+++ airalertbot/bot.py
@@ -1,4 +1,6 @@
-from typing import TYPE_CHECKING
+from logging import getLogger
+from typing import TYPE_CHECKING, cast
+from urllib.parse import urljoin, urlparse
 
 from aiogram import types
 from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
@@ -10,6 +12,8 @@ if TYPE_CHECKING:
     from aiogram import Bot, Dispatcher
     from aiohttp.web import Application
     from dependency_injector.providers import Configuration
+
+logger = getLogger(__name__)
 
 
 @inject
@@ -32,14 +36,26 @@ async def init(
     bot.parse_mode = "HTML"
     dp.include_router(router)
 
-    webhook_path = f"/webhook/bot{bot.token}"
-    webhook_url = f"{config['base_url']}{webhook_path}"
+    base_url = cast(str, config["base_url"])
+    rpath = f"/webhook/bot{bot.token}"
+    parsed_url = urlparse(base_url)
+    if parsed_url.path:
+        # if base_url has path, append webhook path to it
+        webhook_path = parsed_url.path + rpath
+        parsed_url = parsed_url._replace(path=webhook_path)  # noqa: WPS437
+        webhook_url = parsed_url.geturl()
+    else:
+        # if base_url has no path, use webhook path as is
+        webhook_path = rpath
+        webhook_url = urljoin(base_url, webhook_path)
 
     await bot.delete_webhook()
+    logger.debug("Listening at webhook path: %s", webhook_path)
     SimpleRequestHandler(dispatcher=dp, bot=bot).register(app, path=webhook_path)
     setup_application(app, dp)
 
     await bot.set_webhook(webhook_url)
+    logger.info("Registered webhook: %s", webhook_url)
 
     await bot.set_my_commands(
         [
