a5f29057d2dd9c0e0ffd7ebce27cad1bceb9f774
Stanislav <h3xcode@envs.net>
2023-06-28 14:21:11 +0000

    feat: send debug alerts in-place

--- airalertbot/handlers/debug.py
+++ airalertbot/handlers/debug.py
@@ -7,8 +7,10 @@ from aiogram.filters import Command
 from dependency_injector.wiring import Provide, inject
 
 from airalertbot.models import AlarmType, Alert, Status
+from airalertbot.texts import get_text
 
 if TYPE_CHECKING:
+    from aiogram import Bot
     from dependency_injector.providers import Configuration
 
     from airalertbot.services.alerts import AlertsService
@@ -16,12 +18,14 @@ if TYPE_CHECKING:
 logger = getLogger(__name__)
 
 router = Router()
+IMGFILE = types.FSInputFile("assets/map.jpg")
 
 
 @router.message(Command("trigger"))
 @inject
 async def trigger(
     message: types.Message,
+    bot: "Bot" = Provide["bot_context.bot"],
     alerts_service: "AlertsService" = Provide["services.alerts"],
     config: "Configuration" = Provide["bot_context.config"],
 ) -> None:
@@ -29,6 +33,7 @@ async def trigger(
 
     Args:
         message: Message instance.
+        bot: Bot instance.
         alerts_service: Alerts service instance.
         config: Bot configuration instance.
     """
@@ -56,4 +61,14 @@ async def trigger(
         await message.answer("Invalid alert type")
         return
 
-    await alerts_service.trigger_alert(alert)
+    if alert.status == Status.ACTIVATE:
+        await bot.send_photo(
+            message.chat.id,
+            IMGFILE,
+            caption=get_text(alert),
+        )
+    else:
+        await bot.send_message(
+            message.chat.id,
+            get_text(alert),
+        )
