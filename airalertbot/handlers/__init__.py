36f14b8e6368204620c37bb8f02f516282f63bb3
Stanislav <h3xcode@envs.net>
2023-07-23 22:12:59 +0000

    fix: include settings router

--- airalertbot/handlers/__init__.py
+++ airalertbot/handlers/__init__.py
@@ -1,9 +1,10 @@
 """Handlers for the bot."""
 from aiogram import Router
 
-from airalertbot.handlers import basic, debug, weeks
+from airalertbot.handlers import basic, debug, settings, weeks
 
 router = Router()
 router.include_router(basic.router)
 router.include_router(debug.router)
 router.include_router(weeks.router)
+router.include_router(settings.router)
